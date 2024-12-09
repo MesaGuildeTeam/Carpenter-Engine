5676cbd5fbfeff33babf702b60392c38081f13c0
Henderythmix <robertoselleswork@gmail.com>
2024-12-09 15:13:59 -0800

    feat: implemented utility data types and functions

---
+++ src/engine/Utils.cpp
@@ -0,0 +1,53 @@
+#include "Utils.hpp"
+#include <cmath>
+
+#define M_PI 3.14159265358979323846
+
+// 2D Vector Operations
+
+Engine::Vec2f Engine::Vec2f::operator+(const Engine::Vec2f& rhs) {
+  return {x + rhs.x, y + rhs.y};
+}
+
+Engine::Vec2f Engine::Vec2f::operator*(const float& rhs) {
+  return {x * rhs, y * rhs};
+}
+
+Engine::Vec2f Engine::Vec2f::operator*(const Vec2f& rhs) {
+  return {x * rhs.x, y * rhs.y};
+}
+
+// 3D Vector Operations
+
+Engine::Vec3f Engine::Vec3f::operator+(const Engine::Vec3f& rhs) {
+  return {x + rhs.x, y + rhs.y, z + rhs.z};
+}
+
+Engine::Vec3f Engine::Vec3f::operator*(const float& rhs) {
+  return {x * rhs, y * rhs, z * rhs};
+}
+
+Engine::Vec3f Engine::Vec3f::operator*(const Vec3f& rhs) {
+  return {x * rhs.x, y * rhs.y, z * rhs.z};
+}
+
+// Vector Rotations
+
+Engine::Vec2f Engine::Rotate(Vec2f v, float angle) {
+  return {v.x * cos(angle) - v.y * sin(angle), v.x * sin(angle) + v.y * cos(angle)};
+}
+
+Engine::Vec3f Engine::Rotate(Vec3f v, Vec3f angle) {
+  float cosX = cosf(angle.x);
+  float sinX = sinf(angle.x);
+  float cosY = cosf(angle.y);
+  float sinY = sinf(angle.y);
+  float cosZ = cosf(angle.z);
+  float sinZ = sinf(angle.z);
+
+  return {
+    cosY * (sinZ * v.y + cosZ * v.x) - sinY * v.z,
+    sinX * (cosY * v.z + sinY * (sinZ * v.y + cosZ * v.x)) + cosX * (cosZ * v.y - sinZ * v.x),
+    cosX * (cosY * v.z + sinY * (sinZ * v.y + cosZ * v.x)) - sinX * (cosZ * v.y - sinZ * v.x)
+  };
+}
---
+++ src/engine/Utils.hpp
@@ -0,0 +1,130 @@
+/**
+ * @file Utils.hpp
+ * @author Roberto Selles
+ * @brief A collection of utilities for the game engine code
+ */
+
+#ifndef ENGINE_UTILS
+#define ENGINE_UTILS
+
+namespace Engine {
+  /**
+   * A generic success type that determines the stabitility of the code.
+   * This is recommended to be used as voids that need to check for errors
+   * without using try/catch blocks
+   * 
+   * - SUCCESS - The method was run successfully
+   * - FAILURE - The method failed to run
+   * - WARNING - The method was run successfully but with warnings
+   * 
+   * @brief A generic success type
+   * @author Roberto Selles
+   */
+  typedef enum {
+    FAILURE,
+    SUCCESS,
+    WARNING
+  } Success;
+
+  /**
+   * A 2D vector struct with overloaded operators
+   */
+  struct Vec2f {
+    float x;
+    float y;
+
+    // /**
+    //  * Assign a Vec2f
+    //  */
+    // Vec2f operator=(const Vec2f& rhs);
+
+    /**
+     * Add two Vec2f objects together
+     */
+    Vec2f operator+(const Vec2f& rhs);
+
+    // /**
+    //  * Subtract two Vec2f objects
+    //  */
+    // Vec2f operator-(const Vec2f& rhs);
+
+    /**
+     * Compute scalar multiplication
+     */
+    Vec2f operator*(const float& rhs);
+
+    /**
+     * Compute multiplication in parallel
+     */
+    Vec2f operator*(const Vec2f& rhs);
+
+    // /**
+    //  * Dot product calculation
+    //  */
+    // Vec2f operator*(const Vec2f& rhs);
+
+    // /**
+    //  * Scalar division
+    //  */
+    // Vec2f operator/(const float& rhs);
+
+    // /**
+    //  * Negate a Vec2f
+    //  */
+    // Vec2f operator-();
+  };
+
+  /**
+   * A 3D vector struct with overloaded operators
+   */
+  struct Vec3f {
+    float x;
+    float y;
+    float z;
+
+    /**
+     * Adds two Vec3f objects
+     */
+    Vec3f operator+(const Vec3f& rhs);
+
+    /**
+     * Compute scalar multiplication
+     */
+    Vec3f operator*(const float& rhs);
+
+    /**
+     * Computes Multiplication in parallel
+     */
+    Vec3f operator*(const Vec3f& rhs);
+  };
+
+  /**
+   * A color struct with 4 components: RGBA
+   */
+  struct Color {
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+    unsigned char a;
+  };
+
+  /**
+   * Rotates a vector by an angle
+   * 
+   * @param v The vector to rotate
+   * @param angle The angle to rotate
+   * @return The rotated vector
+   */
+  Vec2f Rotate(Vec2f v, float angle);
+
+  /**
+   * Rotates a vector by an angle
+   * 
+   * @param v The vector to rotate
+   * @param angle The angle to rotate
+   * @return The rotated vector
+   */
+  Vec3f Rotate(Vec3f v, Vec3f angle);
+}
+
+#endif
