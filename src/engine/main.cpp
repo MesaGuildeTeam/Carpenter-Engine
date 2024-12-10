da1ebb54f89edb951d26e25d6b2014d673630d26
Henderythmix <robertoselleswork@gmail.com>
2024-12-09 19:56:57 -0800

    test: implemented node test

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -1,8 +1,28 @@
 #include "Game.hpp"
 #include "Window.hpp"
 
+#include <iostream>
+
+class TestScene : public Engine::Scene {
+  public:
+
+    TestScene() : Engine::Scene() {
+      std::cout << "Creating test scene" << std::endl;
+    };
+
+    void Draw() override {
+      std::cout << "Drawing scene" << std::endl;
+      Engine::Node::Draw();
+    };
+
+    void Update(float dt) override {
+      Engine::Node::Update(dt);
+      std::cout << "Updating scene" << std::endl;
+    };
+};
+
 Engine::Window& window{Engine::Window::getWindow()};
-Engine::Game game;
+Engine::Game game(new TestScene());
 
 extern "C" {
   void CallDraw() { game.DrawScene(); }
