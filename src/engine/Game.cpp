b2d1e27f6b46aa370b9e50fbda4096e54850e419
Henderythmix <robertoselleswork@gmail.com>
2024-12-09 19:55:52 -0800

    feat: full implementation of game class

--- src/engine/Game.cpp
+++ src/engine/Game.cpp
@@ -1,13 +1,35 @@
 #include "Game.hpp"
 
-Engine::Game::Game() {
+Engine::Game::Game(Scene* startingScene) {
+  AddScene("Scene0", startingScene);
+  SwitchScene("Scene0");
+}
 
+Engine::Success Engine::Game::AddScene(const char* id, Scene* scene) {
+  if (m_loadedScenes.find(id) != m_loadedScenes.end())
+    return FAILURE;
+  m_loadedScenes.emplace(id, scene);
+  return SUCCESS;
+}
+
+Engine::Success Engine::Game::SwitchScene(const char* id) {
+  if (m_loadedScenes.find(id) == m_loadedScenes.end())
+    return FAILURE;
+  m_currentScene = m_loadedScenes[id];
+  return SUCCESS;
+}
+
+Engine::Success Engine::Game::UnloadScene(const char* id) {
+  if (m_loadedScenes.find(id) == m_loadedScenes.end())
+    return FAILURE;
+  m_loadedScenes.erase(id);
+  return SUCCESS;
 }
 
 void Engine::Game::DrawScene() {
-
+  m_currentScene->Draw();
 }
 
 void Engine::Game::UpdateScene(float dt) {
-
+  m_currentScene->Update(dt);
 }
--- src/engine/Game.hpp
+++ src/engine/Game.hpp
@@ -1,6 +1,9 @@
 #ifndef ENGINE_GAME
 #define ENGINE_GAME
 
+#include "Node.hpp"
+#include <map>
+
 namespace Engine {
   
   /**
@@ -8,14 +11,54 @@ namespace Engine {
    */
   class Game {
     private:
-    // Scene& m_currentScene;
-    // std::map<const char*, Scene*> m_loadedScenes;
-    
-    public:
-    Game();
+    Scene* m_currentScene;
+    std::map<const char*, Scene*> m_loadedScenes;
 
+    public:
+    
+    /**
+     * Constructor with default scene
+     * 
+     * @param startingScene The scene to start the game with
+     */
+    Game(Scene* startingScene);
+
+    /**
+     * Adds a scene to the game
+     * 
+     * @param id The loaded scene ID
+     * @param scene The scene to add
+     * 
+     * @return Success if there is no scene with the same ID
+     */
+    Success AddScene(const char* id, Scene* scene);
+
+    /**
+     * Switches the scene requested
+     * 
+     * @param id The ID of the scene to switch to
+     * @return Success if the scene exists
+     */
+    Success SwitchScene(const char* id);
+
+    /**
+     * Deletes a loaded scene from the game
+     * 
+     * @param id The ID of the scene to delete
+     * @return Success if the scene exists
+     */
+    Success UnloadScene(const char* id);
+
+    // For the game Loop //
+
+    /**
+     * Draws the current scene
+     */
     void DrawScene();
 
+    /**
+     * Updates the current scene
+     */
     void UpdateScene(float dt);
 
   };
