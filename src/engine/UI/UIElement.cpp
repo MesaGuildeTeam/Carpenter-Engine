29549027c53626e1ecedd0c62ac9ccf7eb436ac0
Henderythmix <robertoselleswork@gmail.com>
2024-12-10 20:04:07 -0800

    fix: changed DOM element creation process to actually consider element name instead of just a random number

--- src/engine/UI/UIElement.cpp
+++ src/engine/UI/UIElement.cpp
@@ -9,11 +9,14 @@ Engine::UI::UIElement::UIElement(std::string name) : Engine::Node(name) {
 
 void Engine::UI::UIElement::Init() {
   const char* parentElement = m_parent->m_nodeType == "UI" ? m_parent->m_name.c_str() : "ui-layer";
+  const char* parentClass = m_parent->m_nodeType == "UI" ? ((UI::UIElement*)m_parent)->m_uiClass : "";
 
   EM_ASM({
       var uielement = document.createElement(UTF8ToString($2));
-      uielement.classList.add(UTF8ToString($3));
-      uielement.id = `${UTF8ToString($3)}-${$1}`;
-      document.getElementById(UTF8ToString($0)).appendChild(uielement);
-    }, parentElement, m_name.c_str(), m_uiTag, m_uiClass);
+      uielement.classList.add("ui-element");
+      uielement.classList.add(UTF8ToString($4));
+      console.log(UTF8ToString($0));
+      uielement.id = `${UTF8ToString($4)}-${UTF8ToString($2)}`;
+      document.getElementById(`${UTF8ToString($1) != "" ? UTF8ToString($1) + "-" : ""}${UTF8ToString($0)}`).appendChild(uielement);
+    }, parentElement, parentClass, m_name.c_str(), m_uiTag, m_uiClass);
 }
