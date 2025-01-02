ae39de091d20ed11c549b1e2838b92e2fc69f2d4
Henderythmix <robertoselleswork@gmail.com>
2025-01-01 18:59:49 -0800

    feat: added `Engine::Audio::SoundState`

--- src/engine/Audio/Audio.hpp
+++ src/engine/Audio/Audio.hpp
@@ -1,8 +1,27 @@
+/**
+ * @file Audio.hpp
+ * @brief The basic foundation to all audio classes in the framework
+ */
+
 #ifndef ENGINE_SOUND
 #define ENGINE_SOUND
 
 namespace Engine::Audio {
-  
+ 
+  /**
+   * Enumerator used to represent the state of the audio file
+   * 
+   * States:
+   * - Ready - Song is ready to be queued
+   * - Queued - Song is in the queue but is not playing yet
+   * - Playing - Song is currently playing
+   */
+  typedef enum {
+    Ready,
+    Queued,
+    Playing
+  } SoundState;
+
   /**
    * An abstract class used to hold the information of an audio file.
    * This gets derived into two different classes that should be used instead:
@@ -12,26 +31,26 @@ namespace Engine::Audio {
   class Audio {
     protected:
       
-      const char* m_filename;
+    const char* m_filename;
 
     public:
       
-      /**
-       * Default constructor
-       */
-      Audio(const char* filename); 
+    /**
+     * Default constructor
+     */
+    Audio(const char* filename); 
 
-      /**
-       * Plays the sound
-       */
-      virtual void Play() = 0;
+    /**
+     * Plays the audio file
+     */
+    virtual void Play() = 0;
 
-      /**
-       * Returns if the sound is playing or not
-       * 
-       * @return bool if sound is playing or not
-       */
-      virtual bool playing() = 0;
+    /**
+     * Returns if the sound is playing or not
+     * 
+     * @return bool if sound is playing or not
+     */
+    virtual SoundState playing() = 0;
   };
 };
 
