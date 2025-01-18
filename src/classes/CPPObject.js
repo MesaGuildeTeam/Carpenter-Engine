ed537bab8e7693a0d4ca359b31c79e7118f41999
Henderythmix <robertoselleswork@gmail.com>
2025-01-18 10:54:09 -0800

    fix: added missing dependency

--- src/classes/CPPObject.js
+++ src/classes/CPPObject.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const utils = require("../utils");
 const child_process = require("child_process");
+const os = require("os");
 
 var buildConfig;
 
