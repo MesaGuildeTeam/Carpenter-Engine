37d97f42eb0c6ce7e76e302afcdf418a52acd476
Henderythmix <robertoselleswork@gmail.com>
2025-03-18 00:32:43 -0700

    fix: Images are now loaded at runtime instead of preloaded/embedded

--- src/engine/Graphics/Texture.cpp
+++ src/engine/Graphics/Texture.cpp
@@ -14,8 +14,14 @@ void Engine::Graphics::Texture::LoadTexture() {
   glGenTextures(1, &m_texture);
   glBindTexture(GL_TEXTURE_2D, m_texture);
 
+  unsigned char* data;
+  int size;
+
+  emscripten_wget_data(m_filename, (void**)&data, &size, nullptr);
+
   // Load Image
-  unsigned char* textureData = stbi_load(m_filename, &m_dimensions[0], &m_dimensions[1], nullptr, 4);
+  //unsigned char* textureData = stbi_load(m_filename, &m_dimensions[0], &m_dimensions[1], nullptr, 4);
+  unsigned char* textureData = stbi_load_from_memory(data, size, &m_dimensions[0], &m_dimensions[1], nullptr, 4);
 
   if (textureData == nullptr) {
     std::cerr << "ERROR: Failed to load image name: " << m_filename << std::endl;
