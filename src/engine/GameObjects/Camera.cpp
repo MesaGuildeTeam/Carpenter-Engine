bfd3c2e6beca3fc7fcf4d0bb462df47f4bb1d5de
Roberto Selles <robertoselleswork@gmail.com>
2025-04-22 15:01:49 -0700

    docs: applied license to all new files

--- src/engine/GameObjects/Camera.cpp
+++ src/engine/GameObjects/Camera.cpp
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 #include "Camera.hpp"
 
 Engine::Camera::Camera(std::string name, float fov) : Engine::GameObject(name) {
