b7be732120474587c2649c972040a28c512e0431
Roberto Selles <robertoselleswork@gmail.com>
2025-04-26 00:31:20 -0700

    fix: engine.a is now carpenterengine.a - Corrected .npmignore to only include the build foldeer instead of the objs folder

--- src/build.js
+++ src/build.js
@@ -38,7 +38,7 @@ const outputLocation = buildConfig.outputPath || process.cwd() + "/objs";
 const FrameworkLibrary =
   buildConfig.frameworkPath != null
     ? buildConfig.frameworkPath
-    : "./node_modules/@mesaguilde/carpenter-engine/build/engine.a";
+    : "./node_modules/@mesaguilde/carpenter-engine/build/carpenterengine.a";
 
 const includeDir =
   buildConfig.includeDir != null
@@ -92,7 +92,7 @@ function buildGame(config = defaultBuildSteps) {
     let exec = `${EMCC} ${filesList} ${config.mainFile != "" && config.mainFile != null ? config.mainFile + " -I" + includeDir : ""} ${FrameworkLibrary} -o ./build/engine.js -std=c++20 -sEXPORTED_FUNCTIONS=_Engine_CallUpdate,_Engine_CallDraw -sEXPORTED_RUNTIME_METHODS=ccall,cwrap --bind -sALLOW_MEMORY_GROWTH -sMAX_WEBGL_VERSION=2 -sASYNCIFY ${debugMethods}`;
 
     if (config.libMode == true)
-      exec = `${EMAR} rcs ./build/engine.a ${filesList}`;
+      exec = `${EMAR} rcs ./build/carpenterengine.a ${filesList}`;
     
     utils.execCommand(exec, "Linking Game");
   }
