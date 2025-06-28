dd81beb44d6863cabae0185d7351b618f2323264
Henderythmix <robertoselleswork@gmail.com>
2025-06-27 22:06:00 -0700

    docs: started documentation on UIImage

--- src/engine/UI/UIImage.hpp
+++ src/engine/UI/UIImage.hpp
@@ -9,10 +9,23 @@ namespace Engine::UI {
     std::string m_path;
 
     public:
+
+    /**
+     * @brief Default constructor for a UI Image element
+     * @param name The name of the UI Image element
+     * @param path The path to the image file
+     */
     UIImage(std::string name, std::string path);
 
+    /**
+     * @brief Initializes and instantiates the UI Image element
+     */
     void Init() override;
 
+    /**
+     * @brief Sets the image path for the UI Image element
+     * @param path The path to the image file
+     */
     void SetImage(std::string path);
   };
 }
