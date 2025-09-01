310eaf0516550260698d83896120aca4e1c044b5
Roberto Selles <robertoselleswork@gmail.com>
2025-09-01 12:35:51 -0700

    feat: completed line curve mesh generation - Some meshes will not work well due to aggressive rotations. please be aware when drawing lines such as coils
    fix: restored `HelloWorld.cpp` and created a new `LinesTest.cpp`

--- src/engine/Graphics/Line.cpp
+++ src/engine/Graphics/Line.cpp
@@ -11,45 +11,92 @@ Engine::Graphics::Line::Line(Engine::Vec3f (&lineFunction)(float), float start, 
   // 2. Arc length ||r'(t)||
   float current = start;
 
+  Engine::Vec3f previous = lineFunction(current - interval);
+  Engine::Vec3f currentPoint = lineFunction(current);
+  Engine::Vec3f nextPoint = lineFunction(current + interval);
+  Engine::Vec3f nextNextPoint = lineFunction(current + 2*interval);
+
   while (current <= end) {
-    Engine::Vec3f previous = lineFunction(current - interval);
-    Engine::Vec3f currentPoint = lineFunction(current);
-    Engine::Vec3f nextPoint = lineFunction(current + interval);
-    Engine::Vec3f nextNextPoint = lineFunction(current + 2*interval);
+    Engine::Vec3f deltaStart = nextPoint - previous;
+    Engine::Vec3f deltaEnd = nextNextPoint - currentPoint;
 
-    float thetaStart = atanf((nextPoint.y - previous.y)/(nextPoint.x - previous.x));
-    float thetaEnd = atanf((nextNextPoint.y - currentPoint.y)/(nextNextPoint.x - currentPoint.x));
+    // Calculate the direction of each point
+    // Approximate f(t)' = f(t+i) - f(t-i)/2i
+    // We wont need the 2i here because it's cancelled out in the angle calculation
+    float thetaStart = atanf(deltaStart.y/deltaStart.x);
+    float thetaEnd = atanf(deltaEnd.y/deltaEnd.x);
 
-    float phiStart = atanf((nextPoint.z - previous.z)/(nextPoint.x - previous.x));
-    float phiEnd = atanf((nextNextPoint.z - currentPoint.z)/(nextNextPoint.x - currentPoint.x));
+    float phiStart = acosf(deltaStart.z/sqrt(deltaStart.lengthSquared()));
+    float phiEnd = acosf(deltaEnd.z/sqrt(deltaEnd.lengthSquared()));
 
-    Engine::Vec3f offsetStart = {
+    // Angles created and stored in rotStart (for ring1) and rotEnd (for ring2)
+    // 0: cos(theta)
+    // 1: sin(theta)
+    // 2 cos(phi)
+    // 3: sin(phi)
+    float rotStart[4] = {
       cosf(thetaStart),
       sinf(thetaStart),
       cosf(phiStart),
+      sinf(phiStart)
     };
 
-    Engine::Vec3f offsetEnd = {
+    float rotEnd[4] = {
       cosf(thetaEnd),
       sinf(thetaEnd),
       cosf(phiEnd),
+      sinf(phiEnd)
     };
 
-    // Compute points A, B, C, D as Vec3 and then insert the values into the
-    // vertices
-    //
-    // Structure:
-    //
-    // B-D
-    // | |
-    // A-C
-    Engine::Vec3f a = currentPoint;
-    Engine::Vec3f b = currentPoint - (offsetStart*thickness);
-    Engine::Vec3f d = nextPoint - (offsetEnd*thickness);
-    Engine::Vec3f c = nextPoint;
+    // Create 2 rings and attach the vertices using the polygons
+    // Math has been abstracted. Please look at docs/appA._math.md for more
+    // Details
+    Engine::Vec3f ring1[4]{
+      {rotStart[2]*thickness, rotStart[3]*thickness, 0},
+      {rotStart[3]*rotStart[1]*thickness, -rotStart[2]*rotStart[1]*thickness, rotStart[0]*thickness},
+      {-rotStart[2]*thickness, -rotStart[3]*thickness, 0},
+      {-rotStart[3]*rotStart[1]*thickness, rotStart[2]*rotStart[1]*thickness, -rotStart[0]*thickness}
+    };
 
-    AddQuad({a.x, a.y, a.z, 0.0f, 0.0f}, {c.x, c.y-thickness, c.z, 0.0f, 0.0f},
-      {a.x, a.y-thickness, a.z, 0.0f, 0.0f}, {c.x, c.y, c.z, 0.0f, 0.0f});
+    Engine::Vec3f ring2[4]{
+      {rotEnd[2]*thickness, rotEnd[3]*thickness, 0},
+      {rotEnd[3]*rotEnd[1]*thickness, -rotEnd[2]*rotEnd[1]*thickness, rotEnd[0]*thickness},
+      {-rotEnd[2]*thickness, -rotEnd[3]*thickness, 0},
+      {-rotEnd[3]*rotEnd[1]*thickness, rotEnd[2]*rotEnd[1]*thickness, -rotEnd[0]*thickness}
+    };
+
+    AddQuad(
+      {ring1[0].x + currentPoint.x, ring1[0].y + currentPoint.y, ring1[0].z + currentPoint.z, 0.0f, 0.0f}, 
+      {ring2[0].x + nextPoint.x, ring2[0].y + nextPoint.y, ring2[0].z + nextPoint.z, 0.0f, 0.0f},
+      {ring2[1].x + nextPoint.x, ring2[1].y + nextPoint.y, ring2[1].z + nextPoint.z, 0.0f, 0.0f}, 
+      {ring1[1].x + currentPoint.x, ring1[1].y + currentPoint.y, ring1[1].z + currentPoint.z, 0.0f, 0.0f}
+    );
+
+    AddQuad(
+      {ring1[1].x + currentPoint.x, ring1[1].y + currentPoint.y, ring1[1].z + currentPoint.z, 0.0f, 0.0f}, 
+      {ring2[1].x + nextPoint.x, ring2[1].y + nextPoint.y, ring2[1].z + nextPoint.z, 0.0f, 0.0f},
+      {ring2[2].x + nextPoint.x, ring2[2].y + nextPoint.y, ring2[2].z + nextPoint.z, 0.0f, 0.0f}, 
+      {ring1[2].x + currentPoint.x, ring1[2].y + currentPoint.y, ring1[2].z + currentPoint.z, 0.0f, 0.0f}
+    );
+
+    AddQuad(
+      {ring1[2].x + currentPoint.x, ring1[2].y + currentPoint.y, ring1[2].z + currentPoint.z, 0.0f, 0.0f}, 
+      {ring2[2].x + nextPoint.x, ring2[2].y + nextPoint.y, ring2[2].z + nextPoint.z, 0.0f, 0.0f},
+      {ring2[3].x + nextPoint.x, ring2[3].y + nextPoint.y, ring2[3].z + nextPoint.z, 0.0f, 0.0f}, 
+      {ring1[3].x + currentPoint.x, ring1[3].y + currentPoint.y, ring1[3].z + currentPoint.z, 0.0f, 0.0f}
+    );
+
+    AddQuad(
+      {ring1[3].x + currentPoint.x, ring1[3].y + currentPoint.y, ring1[3].z + currentPoint.z, 0.0f, 0.0f}, 
+      {ring2[3].x + nextPoint.x, ring2[3].y + nextPoint.y, ring2[3].z + nextPoint.z, 0.0f, 0.0f},
+      {ring2[0].x + nextPoint.x, ring2[0].y + nextPoint.y, ring2[0].z + nextPoint.z, 0.0f, 0.0f}, 
+      {ring1[0].x + currentPoint.x, ring1[0].y + currentPoint.y, ring1[0].z + currentPoint.z, 0.0f, 0.0f}
+    );
     current += interval;
+
+    previous = currentPoint;
+    currentPoint = nextPoint;
+    nextPoint = nextNextPoint;
+    nextNextPoint = lineFunction(current + 2*interval);
   }
 }
